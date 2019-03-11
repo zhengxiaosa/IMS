355ceb5ee17c575b96e6d2194c341b1568313b94
Sindera <369341699@qq.com>
2019-03-11 21:13:12 +0800

    TP5

--- public/admin.php
+++ public/admin.php
@@ -14,8 +14,7 @@ namespace think;
 
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
-
+define("BIND_MODULE",'admin');
 // 支持事先使用静态方法设置Request对象和Config对象
-
 // 执行应用并响应
 Container::get('app')->run()->send();
